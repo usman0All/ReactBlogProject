2f087e109556f972d60b994e78d55ebe90ff3f43
mohd usman <usman12797@gmail.com>
2023-09-19 19:11:23 +0530

    5

--- src/component/Content.js
+++ src/component/Content.js
@@ -9,35 +9,25 @@ import i2 from "../image/i.png"
 import i3 from "../image/f.png"
 import i4 from "../image/w.png"
 function Content() {
-  const Cid=useParams().Id
-  console.log("dynamic id",Cid);
-  const[DData]=useContext(Store)
+  const Cid = useParams().Id
+  console.log("dynamic id", Cid);
+  const [DData] = useContext(Store)
   return (
     <div className='dynamicData'>
-    {DData.filter((item)=>item.id===parseInt(Cid)).map((data,index)=>{
-      return(
-      <div key={index} >
-          <h1 className='dhead'>{data.heading.slice(0,50)}</h1>
-          <div className='user' >
-            
-            <img src={image} alt='Not found' className='userimg'/>
-            <div className='userid'>
-            <span>Mohd Usman</span>
-             <span>jan-25/2019</span>
+      {DData.filter((item) => item.id === parseInt(Cid)).map((data, index) => {
+        return (
+          <div key={index} className='parentD1'>
+            <h3 className='dhead'>{data.heading.slice(0, 50)}</h3>
+            <div className='user'>
+            <div className='userImg'><img src={image} className='userImg' alt='Not found'/></div>
+            <div className='userImg'>>Mohammad Usman<span> jan,12/2023</span></div>
             </div>
-            <div className='smedia'>
-               <img src={i1} alt='Not found' className='icon'/>
-               <img src={i2}  alt='Not found' className='icon'/>
-               <img src={i3}   alt='Not found' className='icon'/>
-               <img src={i4}  alt='Not found' className='icon'/>
-             </div>
-          </div>
-          <div className='Dimgbox'>
-          <img src={data.image} className="Dimg" alt="Not found"/>
-          </div>
-          <p>{data.description.slice(0,50)}</p>
-      </div>)
-    })} 
+             <div className='Dimgbox'>
+              <img src={data.image} className="Dimg" alt="Not found" />
+            </div>
+            <p>{data.description.slice(0, 50)}</p>
+          </div>)
+      })}
 
     </div>
   )
